a2733a540ad9c31de51bbf97e1fe1f5a41f0fceb
student <student@BE-403->
2022-07-22 20:46:55 +0800

    巨匠

---
+++ spring.mvc/src/main/java/spring/mvc/session08/controller/HelloController.java
@@ -0,0 +1,17 @@
+package spring.mvc.session08.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping("/welcome")
+	@ResponseBody
+	public String welcome() {
+
+		return "Welcome SpringMVC";
+	}
+}
